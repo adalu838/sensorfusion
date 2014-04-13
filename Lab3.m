51f69c22e66d6f7bc63b08a5e41d6b8f88ace27b
Tobias <tobno568@student.liu.se>
2014-04-13 14:58:16 +0200

    Jobbar med Range parameter trilateration...

--- Lab3.m
+++ Lab3.m
@@ -5,6 +5,24 @@ addpath Z:\sigsys\classes\
 addpath Z:\sigsys\data\
 addpath Z:\sigsys\mfiles\
 load tphat_calibrate
+
+% Velocity of sound:
+v = 34385;
+sensors_good =   [80 0; 
+                  122 36;
+                  100 99;
+                  35 99;
+                  0 72;
+                  0 30;
+                  30 0];
+sensors_bad =    [0 97; 
+                  0 85;
+                  0 74;
+                  0 56;
+                  0 34;
+                  0 17;
+                  0 0];
+              
 %% 1. 
 
 m = mean(tphat, 2);
@@ -41,15 +59,9 @@ pe = ndist(zeros(21,1), diag(variance2));
 sensors = sensormod('h_tdoa',[2 0 21 22]);
 sensors.P = zeros(22,22);
 % Setup sensors
-sensors.th = [80; 0; 
-              122; 36;
-              100; 99;
-              35; 99;
-              0; 72;
-              0; 30;
-              30; 0;
+sensors.th = [reshape(sensors_good',14,1);
               bias(1);bias(2);bias(3);bias(4);bias(5);bias(6);bias(7);
-              34385]';
+              v]';
 sensors.x0 = [67 52];
 sensors.pe = pe;
 
@@ -87,13 +99,7 @@ end
 
 %% Bad configuration
 load tphat_bad
-sensors.th = [0; 97; 
-              0; 85;
-              0; 74;
-              0; 56;
-              0; 34;
-              0; 17;
-              0; 0;
+sensors.th = [reshape(sensors_bad',14,1);
               bias(1);bias(2);bias(3);bias(4);bias(5);bias(6);bias(7);
               34385]';
 %% 
@@ -118,4 +124,26 @@ for sample = 1:5:80
     surface(V)
 end
 
-%% 
+%% Localization: TDOA approach, Range parameter trilateration
+sample = 10;
+sensors = sensors_good;
+
+A = zeros(21,2);
+b = zeros(21,1);
+
+k = 1;
+for m = 1:7
+    for j = (i+1):7
+        
+        t_m0 = (tphat(sample,m)-tphat(sample,0))^2;
+        t_j0 = (tphat(sample,j)-tphat(sample,0))^2;
+        
+        A(k,1) = 2*sensors(m,1)/(v*t_m0) - 2*sensors(j,1)/(v*t_j0);
+        A(k,2) = 2*sensors(m,2)/(v*t_m0) - 2*sensors(j,2)/(v*t_j0);
+        b(k) = - (v*t_m0 - v*t_j0 - (sensors(m,1)^2 + sensors(m,2)^2)/(v*t_m0) + (sensors(j,1)^2 + sensors(j,2)^2)/(v*t_j0));
+        
+        k = k+1;
+    end
+end
+
+x = A\b
